0ea487daf12ba9778c03e84a767f2995800d2b3b
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-10-30 12:18:06 -0700

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const server = require('./server.js');
+const port = 7000;
+ 
+
+
+server.listen(port, () => {
+  console.log("Jordans port is working!!!");
+})
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const express = 'express';
-
+const express = require('express');
+const helmet = require('helmet')
 const server = express();
+
+
+server.use(helmet());
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
+
+
 
 function logger(req, res, next) {
 
